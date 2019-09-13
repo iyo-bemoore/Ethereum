d67a8bde6cd69b1d090e788cc703e7c338fc1fda
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-09-13 15:31:46 +0200

    add refactor

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -2,16 +2,15 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+let accounts
 
 beforeEach( async () => {
-  const req = web3.eth.getAccounts()
-  const res = await req
-  console.log(res) 
+  accounts = await web3.eth.getAccounts()
 })
 
 describe('Inbox', () => {
     it('Should return a list of accounts', () => { 
-
+       console.log(accounts)
     })
 })
 
