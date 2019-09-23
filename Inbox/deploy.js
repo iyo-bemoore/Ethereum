59c1833150e480d1c3b0a201153af7e42e49b287
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-09-23 14:05:13 +0200

    add deploy

---
+++ Inbox/deploy.js
@@ -0,0 +1,4 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+
+const { interface, bytecode  } = require('./compile');
--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -2,26 +2,43 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
 const { interface, bytecode } = require('../compile');
 
 
 let accounts
 let inbox;
+const INITIAL_MESSAGE = "Deployed"
+const InfuraApi = "rinkeby.infura.io/v3/f2205ed54d144eadae878f6e5ae7e731"
 
-beforeEach( async () => {
-
+beforeEach(async () => {
   // get the list of the accounts
   accounts = await web3.eth.getAccounts()
   // use one of the account numbers to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-          .deploy({data: bytecode, arguments: ['Hello There']})
-          .send({from: accounts[0], gas: '1000000'})
+    .deploy({ data: bytecode, arguments: ['hi there'] })
+    .send({ from: accounts[0], gas: '1000000' })
 })
 
 describe('Inbox', () => {
-    it('Deploys a contract', () => { 
-       assert.ok(inbox.options.address)
-    })
+  it('Deploys a contract', () => {
+    assert.ok(inbox.options.address)
+  });
+
+  it('should have a message', () => {
+    return async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'hi there');
+    }
+  });
+
+  it('should update the message', () => {
+    return async () => {
+      await inbox.methods.setMessage('OK').send({ from: accounts[0] });
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'OK');
+    }
+  })
 })
 
 
