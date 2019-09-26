09fa39f538556f26e39c566c561bab1a6148415d
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-09-26 16:30:54 +0200

    add test scripts

--- Inbox/deploy.js
+++ Inbox/deploy.js
@@ -7,9 +7,10 @@ const provider = new HDWalletProvider(mNic, endPoint);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-  console.log("Attempting account on ", accounts[0]);
 
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting account on ", accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: "0x" + bytecode,
