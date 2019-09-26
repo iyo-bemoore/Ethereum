f44f14f43aed2438bd14a12abc0e8577f43ba702
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-09-26 16:27:46 +0200

    write test out

--- Lottery/test/lottery.test.js
+++ Lottery/test/lottery.test.js
@@ -1,12 +1,9 @@
-// Build in Node.js module
-const assert = require("assert");
-
-// Local test network
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-const { interface, bytecode } = require("../compile");
+const { interface, bytecode } = require('../compile');
 
 let lottery;
 let accounts;
@@ -14,6 +11,16 @@ let accounts;
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+    .deploy({ data:bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 });
+
+describe('Lottery', () => {
+  it('Deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  })
+
+
+})
+
+
