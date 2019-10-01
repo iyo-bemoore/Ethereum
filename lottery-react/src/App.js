57e528a9d9f3194b0ee487a2e2d7b4ecc783a247
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-10-01 20:45:43 +0200

    refactor app

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,31 +1,60 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import lottery from "./lottery";
 import web3 from "./web3";
 import Header from "./components/Header";
 import Form from "./components/Form";
 
-class App extends Component {
+// class App extends Component {
   
-  state = { 
-    manager: "",
-    players:[],
-    balance:'' 
-  };
+//   state = { 
+//     manager: "",
+//     players:[],
+//     balance:'' 
+//   };
   
-  async componentDidMount() {
+//   async componentDidMount() {
+//     const manager = await lottery.methods.manager().call();
+//     const players = await lottery.methods.getPlayers().call();
+//     const balance = await web3.eth.getBalance(lottery.options.address)
+//     this.setState({ manager,players,balance });
+//   }
+//   render() {
+//     return (
+//       <div className="wrapper">
+//         <Header/>
+//         <Form/>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+const [manager, setManager] = useState("")
+const [balance, setBalance] = useState("")
+const [players, setPlayers] = useState([])
+
+useEffect(() => {
+  async function getLotteryData(){
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address)
-    this.setState({ manager,players,balance });
+    setManager(manager);
+    setBalance(balance);
+    setPlayers([...players, players]);
   }
-  render() {
-    return (
-      <div className="wrapper">
-        <Header/>
-        <Form/>
-      </div>
-    );
-  }
+  getLotteryData();
+},[])
+ 
+
+  return(
+    <>
+    <Header/>
+     <div>{manager}</div>
+     <div>{balance}</div>
+     <div>{players.length}</div>
+    <Form/>
+   </>
+  )
 }
 
 export default App;
